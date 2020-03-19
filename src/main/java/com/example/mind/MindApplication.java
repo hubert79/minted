9fc1c10c64ff70c1323980a3893c25811a2acc4b
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2020-03-19 15:48:17 +0100

    początek prac

---
+++ src/main/java/com/example/mind/MindApplication.java
@@ -0,0 +1,13 @@
+package com.example.mind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mind/MindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
